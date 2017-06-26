1ee326e8b98b31058669a904763faff5b7691e66
Rostyslav Midenko <rmidenko2012@gmail.com>
2017-06-26 13:53:04 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ var a = Number(prompt('Enter a'));
 var b = Number(prompt('Enter b'));
 var c = Number(prompt('Enter c'));
 
-var result = quadraticEquation(a, b, c);
+document.write('a= ' + a +';');
+document.write('b= ' + b +';');
+document.write('c= ' + c +';');
+document.write('<br>');
+
+document.write('Equation: ' + a + 'x <sup>2</sup> + ' + b + 'x +  ' + c);
+document.write('<br>');var result = quadraticEquation(a, b, c);
 document.write(result);
 
 function quadraticEquation(a, b, c) {
@@ -14,8 +20,8 @@ function quadraticEquation(a, b, c) {
 
 
 var d = calcD(a, b, c);
-var x1 = (-b + Math.sqrt(d)) / 2 / a;
-var x2 = (-b - Math.sqrt(d)) / 2 / a;
+var x1 = Math.round((-b + Math.sqrt(d)) / 2 / a);
+var x2 = Math.round((-b - Math.sqrt(d)) / 2 / a);
 return 'x1 = ' + x1 + ', x2 = ' + x2;
 }
 function calcD(a, b, c) {
